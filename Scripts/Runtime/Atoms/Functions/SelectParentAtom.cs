1972ab8d9dc9491a98a0f7839b613065b1771ec6
niscolas <nic.cp@hotmail.com>
2021-08-06 00:23:23 -0300

    adds some Scripts and Atoms from UnityUtils.Atoms

---
+++ Scripts/Runtime/Atoms/Functions/SelectParentAtom.cs
@@ -0,0 +1,17 @@
+﻿using UnityAtoms.MonoHooks;
+using UnityEngine;
+
+namespace niscolas.UnityUtils.UnityAtoms
+{
+    [CreateAssetMenu
+    (
+        menuName = Constants.FunctionsCreateAssetMenuPath + "(GameObject) => Select Parent : GameObject"
+    )]
+    public class SelectParentAtom : GameObjectGameObjectFunction
+    {
+        public override GameObject Call(GameObject gameObject)
+        {
+            return gameObject.transform.parent.gameObject;
+        }
+    }
+}
