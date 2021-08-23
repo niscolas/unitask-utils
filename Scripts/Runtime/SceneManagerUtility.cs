2650450d5e720383dc86b7e0cc10c3cc6ca2661a
niscolas <nic.cp@hotmail.com>
2021-08-23 12:53:21 -0300

    adds UNITY_EDITOR checks

--- Scripts/Runtime/SceneManagerUtility.cs
+++ Scripts/Runtime/SceneManagerUtility.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
 using UnityAtoms.SceneMgmt;
+#if UNITY_EDITOR
 using UnityEditor.SceneManagement;
+#endif
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -8,12 +10,14 @@ namespace BestLostNFound
 {
     public static class SceneManagerUtility
     {
+#if UNITY_EDITOR
         private static readonly Dictionary<LoadSceneMode, OpenSceneMode> LoadSceneToOpenSceneModeMapping =
             new Dictionary<LoadSceneMode, OpenSceneMode>
             {
                 {LoadSceneMode.Single, OpenSceneMode.Single},
                 {LoadSceneMode.Additive, OpenSceneMode.Additive}
             };
+#endif
 
         public static void LoadScenes(IEnumerable<SceneField> scenes, LoadSceneMode loadSceneMode)
         {
@@ -25,14 +29,14 @@ namespace BestLostNFound
 
         public static void LoadScene(SceneField scene, LoadSceneMode loadSceneMode)
         {
+#if UNITY_EDITOR
             if (!Application.isPlaying)
             {
                 EditorSceneManager.OpenScene(scene.ScenePath, LoadSceneToOpenSceneModeMapping[loadSceneMode]);
+                return;
             }
-            else
-            {
-                SceneManager.LoadScene(scene.SceneName, loadSceneMode);
-            }
+#endif
+            SceneManager.LoadScene(scene.SceneName, loadSceneMode);
         }
     }
 }
