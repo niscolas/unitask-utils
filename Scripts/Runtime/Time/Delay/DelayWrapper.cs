4afce639f480dfa46841a9c6c26cc08866accc02
niscolas <nic.cp@hotmail.com>
2021-08-24 10:15:58 -0300

    fix "With Cancellation" not found

--- Scripts/Runtime/Time/Delay/DelayWrapper.cs
+++ Scripts/Runtime/Time/Delay/DelayWrapper.cs
@@ -6,38 +6,38 @@ using UnityEngine;
 
 namespace UnityUtils
 {
-	[Serializable]
-	public struct DelayWrapper
-	{
-		[SerializeField]
-		private float _seconds;
+    [Serializable]
+    public struct DelayWrapper
+    {
+        [SerializeField]
+        private float _seconds;
 
-		[SerializeField]
-		private int _frames;
+        [SerializeField]
+        private int _frames;
 
-		public float Seconds
-		{
-			get => _seconds;
-			set => _seconds = value;
-		}
+        public float Seconds
+        {
+            get => _seconds;
+            set => _seconds = value;
+        }
 
-		public int Frames
-		{
-			get => _frames;
-			set => _frames = value;
-		}
+        public int Frames
+        {
+            get => _frames;
+            set => _frames = value;
+        }
 
-		public IEnumerator Delay()
-		{
-			if (_seconds > 0)
-			{
-				yield return new WaitForSeconds(_seconds);
-			}
+        public IEnumerator Delay()
+        {
+            if (_seconds > 0)
+            {
+                yield return new WaitForSeconds(_seconds);
+            }
 
-			for (int i = 0; i < _frames; i++)
-			{
-				yield return null;
-			}
-		}
-	}
+            for (int i = 0; i < _frames; i++)
+            {
+                yield return null;
+            }
+        }
+    }
 }
--- Scripts/Runtime/Time/Delay/DelayedUnityEvent.cs
+++ Scripts/Runtime/Time/Delay/DelayedUnityEvent.cs
@@ -5,18 +5,18 @@ using UnityEngine.Events;
 
 namespace UnityUtils
 {
-	public class DelayedUnityEvent : MonoBehaviour
-	{
-		[SerializeField]
-		private FloatReference _delaySec;
+    public class DelayedUnityEvent : MonoBehaviour
+    {
+        [SerializeField]
+        private FloatReference _delaySec;
 
-		[SerializeField]
-		private UnityEvent _event;
+        [SerializeField]
+        private UnityEvent _event;
 
-		public async void Do()
-		{
-			await Await.Seconds(_delaySec, gameObject);
-			_event?.Invoke();
-		}
-	}
+        public async void Do()
+        {
+            await Await.Seconds(_delaySec, gameObject);
+            _event?.Invoke();
+        }
+    }
 }
