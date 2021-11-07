e7805d79ae4142179fd9088ff6d2b6f4f6be71df
niscolas <nic.cp@hotmail.com>
2021-11-07 18:13:13 -0300

    adds new features

---
+++ Scripts/Runtime/Atoms/Actions/SetMaterialFloatPropertyDynamicAtomAction.cs
@@ -0,0 +1,23 @@
+﻿using niscolas.UnityUtils.UnityAtoms;
+using UnityAtoms;
+using UnityAtoms.BaseAtoms;
+using UnityEngine;
+
+namespace niscolas.UnityUtils.Extras
+{
+    [CreateAssetMenu(
+        menuName = UnityAtomsConstants.ActionsCreateAssetMenuPrefix + "(float) => Set Material Float Property")]
+    public class SetMaterialFloatPropertyDynamicAtomAction : AtomAction<float>
+    {
+        [SerializeField]
+        private StringReference _propertyName;
+        
+        [SerializeField]
+        private Material _material;
+        
+        public override void Do(float value)
+        {
+            _material.SetFloat(_propertyName.Value, value);
+        }
+    }
+}
