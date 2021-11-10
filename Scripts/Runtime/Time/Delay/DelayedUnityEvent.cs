0d0fcb3e2061fe136f2650c82645ede80722ff14
niscolas <nic.cp@hotmail.com>
2021-11-10 15:13:10 -0300

    updates DelayedUnityEvent

--- Scripts/Runtime/Time/Delay/DelayedUnityEvent.cs
+++ Scripts/Runtime/Time/Delay/DelayedUnityEvent.cs
@@ -1,21 +1,27 @@
-﻿using niscolas.UnityUtils.Extras;
+﻿using niscolas.OdinCompositeAttributes;
+using niscolas.UnityUtils.Extras;
 using UnityAtoms.BaseAtoms;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.Serialization;
 
 namespace UnityUtils
 {
     public class DelayedUnityEvent : MonoBehaviour
     {
+        [FormerlySerializedAs("_delaySec"), SecondsLabel, SerializeField]
+        private FloatReference _secondsDelay;
+        
         [SerializeField]
-        private FloatReference _delaySec;
+        private IntReference _framesDelay;
 
         [SerializeField]
         private UnityEvent _event;
 
         public async void Do()
         {
-            await Await.Seconds(_delaySec, gameObject);
+            await Await.Frames(_framesDelay.Value);
+            await Await.Seconds(_secondsDelay, gameObject);
             _event?.Invoke();
         }
     }
