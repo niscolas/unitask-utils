61dd063457161942bbc61c13b6cdf6cae35520b9
niscolas <nic.cp@hotmail.com>
2021-11-11 18:02:25 -0300

    adds SimpleApplyForce

---
+++ Scripts/Runtime/Physics/SimpleApplyForce.cs
@@ -0,0 +1,29 @@
+﻿using niscolas.UnityUtils.Core;
+using Sirenix.OdinInspector;
+using UnityAtoms.BaseAtoms;
+using UnityEngine;
+
+namespace niscolas.UnityUtils.Extras
+{
+    public class SimpleApplyForce : CachedMonoBehaviour
+    {
+        [SerializeField]
+        private FloatReference _force = new FloatReference(1);
+
+        [SerializeField]
+        private ForceMode _forceMode;
+        
+        [Required, SerializeField]
+        private Transform _forceDirectionReference;
+
+        public void Do(GameObject target)
+        {
+            if (!target.TryGetComponent(out Rigidbody targetRigidbody))
+            {
+                return;
+            }
+            
+            targetRigidbody.AddForce(_force.Value * _forceDirectionReference.forward, _forceMode);
+        }
+    }
+}
